cbcd25e1626e69d6f8309a51307229e84bd51f22
Barissa-Imran <imranbarissa@gmail.com>
2023-08-03 23:09:28 +0300

    add 0. Basic annotations - add

---
+++ alx-backend-python/0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Module that provides a function for adding two numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Parameters:
+        a (float): The first number.
+        b (float): The second number.
+
+    Returns:
+        float: The sum of a and b.
+    """
+    return a + b
