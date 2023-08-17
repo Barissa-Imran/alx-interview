490aaffab48e640bfb9529e9300cce6800d2ef47
Barissa-Imran <imranbarissa@gmail.com>
2023-08-17 20:15:24 +0300

    fix 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """
-log parsing algorithm
+Log parsing
 """
 
 import sys
-
 
 if __name__ == '__main__':
 
@@ -12,10 +11,7 @@ if __name__ == '__main__':
     codes = ["200", "301", "400", "401", "403", "404", "405", "500"]
     stats = {k: 0 for k in codes}
 
-    def print_stats(stats: dict, file_size:int) -> None:
-        """
-        print stats given
-        """
+    def print_stats(stats: dict, file_size: int) -> None:
         print("File size: {:d}".format(filesize))
         for k, v in sorted(stats.items()):
             if v:
